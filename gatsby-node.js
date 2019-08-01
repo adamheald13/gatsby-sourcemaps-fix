cb491764eeb8def00b9a450752d4d39d6d775342
Adam Heald <adamheald13@gmail.com>
2019-08-01 17:41:19 -0500

    Use webpack.SourceMapDevToolPlugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+const webpack = require('webpack');
+
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    devtool: false,
+    plugins: [
+      // Fix gatsby's seemingly broken sourcemaps
+      new webpack.SourceMapDevToolPlugin({})
+    ],
+  })
+}
